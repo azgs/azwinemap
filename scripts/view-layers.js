b6bf4e84b1fc0a32ab0c0306a696a1294cf11e96
jessica-azgs <jessica.good@azgs.az.gov>
2015-10-05 14:09:02 -0700

    added open now button

--- scripts/view-layers.js
+++ scripts/view-layers.js
@@ -26,7 +26,7 @@ app.views.FarmsView = Backbone.View.extend({
   },
   events: {
     "click a": "switchLayers",
-    "click button": "toggleLayers",
+    "click button": "toggleLayers"
   },
   addToMap: function () {
     this.model.get("layer").addTo(app.map);
@@ -41,26 +41,60 @@ app.views.FarmsView = Backbone.View.extend({
     }
     $(this.el).append(template({
       model: model,
-      wines: wines,
+      wines: wines
     }))
   },
   filterJSON: function (layer, watcher) {
     layer.setFilter(function (feature) {
       var wines = feature.properties.type;
-      for (key in wines) {
-        if (wines[key] == true)
-          if (watcher.indexOf(key) !== -1)
+
+      // Filter by open now
+      if (watcher.indexOf("open") !== -1) {
+        var date = new Date();
+        var day = date.getDay();
+        var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+        var open_hours = feature.properties.hours[days[day]];
+
+        if (open_hours.open.match(/^\d\d?:\d\d/g) && open_hours.close.match(/^\d\d?:\d\d/g)) {
+          var open_time = new Date();
+          open_time.setHours(open_hours.open.split(":")[0]);
+          open_time.setMinutes(open_hours.open.split(":")[1].split(" ")[0]);
+          open_time.setSeconds(0);
+          open_time.setMilliseconds(0);
+          if (open_hours.open.match(/pm$/g) && open_time.getHours() < 12)
+            open_time.setHours(open_time.getHours() + 12);
+          if (open_hours.open.match(/am$/g) && open_time.getHours() == 12)
+            open_time.setHours(open_time.getHours() - 12);
+          var close_time = new Date();
+          close_time.setHours(open_hours.close.split(":")[0]);
+          close_time.setMinutes(open_hours.close.split(":")[1].split(" ")[0]);
+          close_time.setSeconds(0);
+          open_time.setMilliseconds(0);
+          if (open_hours.close.match(/pm$/g) && close_time.getHours() < 12)
+            close_time.setHours(close_time.getHours() + 12);
+          if (open_hours.close.match(/am$/g) && close_time.getHours() == 12)
+            close_time.setHours(close_time.getHours() - 12);
+
+          if (date.getTime() >= open_time.getTime() && date.getTime() < close_time.getTime())
             return feature;
+        }
+      }
+      else {
+        for (key in wines) {
+          if (wines[key] == true)
+            if (watcher.indexOf(key) !== -1)
+              return feature;
+        }
       }
     })
   },
   toggleLayers: function (e) {
     var self = this;
     var layer = this.model.get('layer');
-    var wines = this.model.get('wines')
+    var wines = this.model.get('wines');
     var filter = e.currentTarget.id;
     var target = $(e.currentTarget);
-    var winesList = []
+    var winesList = [];
 
     for (key in wines) {
       winesList.push(wines[key]['id']);
@@ -68,6 +102,7 @@ app.views.FarmsView = Backbone.View.extend({
 
     var allID = $('#all');
     var cropsID = $("#toggle-crops button");
+    var openID = $("#toggle-open button");
 
     if (filter === "all") {
       self.active = [];
@@ -77,9 +112,22 @@ app.views.FarmsView = Backbone.View.extend({
       } else {
         self.filterJSON(layer, winesList);
         cropsID.removeClass("active");
+        openID.removeClass("active");
         target.addClass("active");
       }
-    } else {
+    }
+    else if (filter === "open") {
+      if (target.hasClass("active")) {
+        self.filterJSON(layer, []);
+        target.removeClass("active");
+      } else {
+        target.addClass("active");
+        cropsID.removeClass("active");
+        allID.removeClass("active");
+        self.filterJSON(layer, filter);
+      }
+    }
+    else {
       if (target.hasClass("active")) {
         var index = self.active.indexOf(filter);
         if (index !== -1) {
@@ -90,6 +138,7 @@ app.views.FarmsView = Backbone.View.extend({
       } else {
         target.addClass("active");
         allID.removeClass("active");
+        openID.removeClass("active");
         var index = self.active.indexOf(filter);
         if (index === -1) {
           self.active.push(filter);
